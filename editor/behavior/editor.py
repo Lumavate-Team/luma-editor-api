44233d6180222581d9883a9660a57ed2a9ed5840
Gunnar Norred <gnorred88@gmail.com>
2019-04-01 18:59:53 +0000

    Add create and delete methods

--- editor/behavior/editor.py
+++ editor/behavior/editor.py
@@ -1,10 +1,11 @@
 from lumavate_service_util import RestBehavior
 from flask import jsonify, request, g
 from pathlib import Path
+import shutil
 import os
 
 class EditorBehavior(RestBehavior):
-  def __init__(self):
+  def __init__(self, data=None, args=None):
     self.project_config = {
         'widget': '/app/',
         'dep': '/other_path/'
@@ -15,15 +16,36 @@ class EditorBehavior(RestBehavior):
   def get_info(self):
     return jsonify(self.project_config)
 
-  def validate_path(self, root, path):
+  def append_slash(self, path):
+    if not path.endswith('/'):
+      return path + '/'
+
+    return path
+
+  def format_real_path(self, root, path):
     req_root = self.project_config.get(root)
-    if not req_root:
-      raise Exception('Path does not exsist')
 
     if path:
       new_path = '{}{}'.format(req_root, path)
     else:
       new_path = req_root
+
+    return new_path
+
+  def format_editor_path(self, root, req_path):
+    editor_path = '{}{}'.format(self.append_slash(root), req_path)
+
+    if os.path.isdir(self.format_real_path(root, req_path)):
+      return self.append_slash(editor_path)
+    else:
+      return editor_path
+
+  def validate_path(self, root, path):
+    req_root = self.project_config.get(root)
+    if not req_root:
+      raise Exception('Path does not exsist')
+
+    new_path = self.format_real_path(root, path)
 
     if not os.path.exists(new_path):
       raise Exception('Path does not exsist')
@@ -37,13 +59,11 @@ class EditorBehavior(RestBehavior):
     if os.path.isfile(path):
       res = {
           "path": path,
-          'root': self.project_config.get(root),
           "type": 1
           }
     elif os.path.isdir(path):
       res = {
           "path": path,
-          'root': self.project_config.get(root),
           "type": 2
           }
     else:
@@ -52,15 +72,18 @@ class EditorBehavior(RestBehavior):
     return jsonify(res)
 
   def read(self, root, req_path):
+    if 'stat' in request.args:
+      return self.stat(root, req_path)
+
     path = self.validate_path(root, req_path)
+    editor_path = self.format_editor_path(root, req_path)
 
     res = None
     if os.path.isfile(path):
       contents = Path(path).read_text()
       res = {
-          "path": req_path,
+          "path": editor_path,
           "contents": contents,
-          'root': self.project_config.get(root),
           "type": 1
           }
     elif os.path.isdir(path):
@@ -68,14 +91,13 @@ class EditorBehavior(RestBehavior):
       contents = []
 
       for entry in dir_contents:
-        full_path = '{}{}'.format(self.append_slash(path), entry)
+        full_path = '{}{}'.format(editor_path, entry)
         is_file = os.path.isfile(full_path)
-        contents.append({'name': entry, 'path': self.append_slash(req_path), 'type': 1 if is_file else 2, 'contents': None})
+        contents.append({'name': entry, 'path': full_path, 'type': 1 if is_file else 2, 'contents': None})
 
       res = {
-          "path": self.append_slash(req_path),
+          "path": editor_path,
           "contents": contents,
-          'root': self.project_config.get(root),
           "type": 2
           }
     else:
@@ -83,29 +105,49 @@ class EditorBehavior(RestBehavior):
 
     return jsonify(res)
 
-  def append_slash(self, path):
-    if not path.endswith('/'):
-      return path + '/'
-
-    return path
-
   def create(self, root, path):
     req_root = self.project_config.get(root)
     if not req_root:
-      raise Exception('Path does not exsist')
+      raise Exception('Root path does not exsist')
 
-    if path:
-      new_path = '{}{}'.format(req_root, path)
+    # Can't overwite root path
+    if not path:
+      raise Exception('Path already exsists')
     else:
-      new_path = req_root
+      new_path = self.format_real_path(root, path)
 
     if os.path.exists(new_path):
       raise Exception('Path already exsists')
 
-    os.mkdir(new_path)
+    try:
+      data = self.get_data()
+      if data.get('type') == 'directory':
+        os.makedirs(new_path)
+      elif data.get('type') == 'file':
+        file_name = path.split('/')[-1]
+        file_path = '/'.join(path.split('/')[:-1])
+
+        if not os.path.exists(self.format_real_path(root, file_path)):
+          os.makedirs(self.format_real_path(root, file_path))
+          open(new_path, 'a').close()
+        else:
+          open(new_path, 'a').close()
+
+      return self.read(root, path)
+
+    except Exception as e:
+      return jsonify({"status": 'failed'})
 
   def delete(self, root, path):
-    pass
+    self.validate_path(root, path)
+    path = self.format_real_path(root, path)
+
+    if os.path.isfile(path):
+      os.remove(path)
+    elif os.path.isdir(path):
+      shutil.rmtree(path)
+
+    return jsonify('ok')
 
   def write(self, root, path):
     path = self.validate_path(root, path)
--- editor/routes/editor.py
+++ editor/routes/editor.py
@@ -8,9 +8,6 @@ editor_blueprint = Blueprint("editor_blueprint", __name__)
 @lumavate_blueprint.route('/<string:ic>/<string:wt>/luma-editor/fs/<string:root>/<path:path>', methods=['GET', 'PUT', 'POST', 'DELETE'])
 def editor_core(ic, wt, root, path):
   b = behavior.EditorBehavior()
-  if 'stat' in request.args:
-    return b.stat(root, path)
-
   methods = {
       'GET': b.read,
       'PUT': b.write,
@@ -19,7 +16,3 @@ def editor_core(ic, wt, root, path):
       }
 
   return methods[request.method](root, path)
-
-@lumavate_blueprint.route('/<string:ic>/<string:wt>/luma-editor/proj/info', methods=['GET'])
-def info(ic, wt):
-  return behavior.EditorBehavior().get_info()
